b4368e40acacc8cd92a009a9a00f679a013bf8a2
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-10 13:27:27 -0500

    declare var

--- MyFirstWebApp/MyFirstWebApp/Default.aspx.cs
+++ MyFirstWebApp/MyFirstWebApp/Default.aspx.cs
@@ -16,12 +16,24 @@ namespace MyFirstWebApp
 
         protected void button1_Click(object sender, EventArgs e)
         {
-            string firstName = firstNameTextBox.Text;
+            //string firstName = firstNameTextBox.Text;
+            // https://msdn.microsoft.com/en-us/library/cs7y5x0x%28v=vs.90%29.aspx?f=255&MSPPError=-2147217396
+            //link to check data type
+
+            string firstName;
+            firstName = "Bob";
+            resultLabel.Text = firstName;
+
+            /*
+
+
             string lastName = lastNameTextBox.Text;
 
             string result = "Hello " + firstName + " " + lastName;
 
             resultLabel.Text = result;
+
+            */
         }
     }
 }
