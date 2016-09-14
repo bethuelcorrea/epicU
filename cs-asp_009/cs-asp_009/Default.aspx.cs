1048a9fc3b9a5ab84125b50780e0dd9e0bd07b62
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-14 15:34:45 -0500

    operations

---
+++ cs-asp_009/cs-asp_009/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cs_asp_009
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //double myDouble =5.5;
+            //int myInteger = 7;
+            //int myOtherInteger=6;
+
+            // double myresult =(double) myInteger / (double)myOtherInteger;
+            // lbltext.Text = myresult.ToString();
+
+
+            int firstnumber  = 2000000000;
+            int secondnumber =  2000000000;
+            //long resultNumber = firstnumber * secondnumber;
+
+            long resultNumber;
+
+            checked {
+                
+                     }
+
+
+        }
+    }
+}
---
+++ cs-asp_009/cs-asp_009/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cs_asp_009.Global" Language="C#" %>
