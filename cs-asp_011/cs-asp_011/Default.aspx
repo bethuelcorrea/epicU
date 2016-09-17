655feb44245efd4eb5c9a4b5ed22d5ef240dd112
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-17 09:30:35 -0500

    cs.asp_011 project

---
+++ cs-asp_011/cs-asp_011/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="cs_asp_011.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Is<asp:TextBox ID="firstTextBox1" runat="server"></asp:TextBox>
+        equal to<asp:TextBox ID="secondlTextBox2" runat="server"></asp:TextBox>
+        ?<br />
+        <br />
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="are you cool?" />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="ok" />
+        <br />
+        <br />
+        <asp:Label ID="lbltxt" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ cs-asp_011/cs-asp_011/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cs_asp_011
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            //lbltxt.Text = "";
+
+            //  if (firstTextBox1.Text == secondlTextBox2.Text)
+            // {
+            //      lbltxt.Text = "yes they are equal";
+            //   }
+            //   else {
+
+            //       lbltxt.Text = "no not equal";
+            //   }
+
+            /*
+            if (CheckBox1.Checked)
+            {
+
+                lbltxt.Text = "yes you are cool";
+            }
+            else {
+
+                lbltxt.Text = "No you are not cool";
+            }
+
+            */
+
+            //if (pizzaRadioBotton.checked){
+
+            //result.txt = "you must be from keene"
+
+            // } else if(){}
+
+
+
+
+
+
+        }
+    }
+}
