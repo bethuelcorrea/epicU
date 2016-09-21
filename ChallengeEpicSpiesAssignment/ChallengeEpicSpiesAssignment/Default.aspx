3df71a37646669c66730772773b854c9252b6b70
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-21 15:03:46 -0500

    assignment

--- ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx
@@ -5,11 +5,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        
+    </style>
+    <link type="text/css" rel="stylesheet" href="main.css"/>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
     
+        <asp:Image ID="Image1" runat="server" Height="190px" ImageUrl="epic-spies-logo.jpg" />
+        <br />
+        <br />
+        <span class="auto-style1">Spy New Assignment Form<br />
+        </span>
+        <br />
+        Spy Code Name:<asp:TextBox ID="codeName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        New Assignment Name:<asp:TextBox ID="assignname" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        End Date of Previous Assignment<asp:Calendar ID="endCurrentAssignment" runat="server"></asp:Calendar>
+        <br />
+        Start Date of New Assignment<br />
+        <asp:Calendar ID="startNewAssignment" runat="server"></asp:Calendar>
+        <br />
+        Projected End Date of New Assingment.<br />
+        <asp:Calendar ID="projectedAssignment" runat="server"></asp:Calendar>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        <br />
+        <br />
+        <asp:Label ID="lblresult" runat="server"></asp:Label>
+        <br />    
     </div>
     </form>
 </body>
--- ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -11,6 +11,70 @@ namespace ChallengeEpicSpiesAssignment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack) {
+
+                // this will set the default date on the calenders
+                codeName.Text = " ";
+                endCurrentAssignment.SelectedDate = DateTime.Now.Date;
+                startNewAssignment.SelectedDate= DateTime.Now.Date.AddDays(14);
+                projectedAssignment.SelectedDate = DateTime.Now.Date.AddDays(21);               
+
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            DateTime endDatesch = endCurrentAssignment.SelectedDate;
+            DateTime startNewSch = startNewAssignment.SelectedDate;
+            DateTime proAssignshedule = projectedAssignment.SelectedDate;
+
+
+            
+
+            //this is the TimeSpan from last calender - second caleder 7 days 
+
+            TimeSpan totaltime =  proAssignshedule - startNewSch ;
+
+            double cost = totaltime.Days * 500;                      
+
+            if (totaltime.Days > 21) {
+
+                cost = (totaltime.Days * 500) + 1000;
+            }
+
+            //not allow time under the two week check
+            TimeSpan notAllowTime = startNewSch - endDatesch;
+
+            if (notAllowTime.Days < 14)
+            {
+                lblresult.Text = "<font color='red'>Error:</font> Must allow at least two weeks between previous assignment and new assignment";
+                startNewAssignment.SelectedDate = DateTime.Now.Date.AddDays(14);
+            }
+            else
+            {  //final stage output
+
+
+                //validation of empty string before output.
+                if (assignname.Text.Length <= 1 || codeName.Text.Length <= 1)
+                        {
+                             lblresult.Text = "Code Name and Assignment Name are required!";
+                        }
+                else {
+
+                    //this is my check just to make your my day are right
+                    // lblresult.Text = cost.ToString() + "<br/>" + totaltime.Days + "<br/>" + notAllowTime.Days;
+                    lblresult.Text = string.Format("&nbsp;Assignment of {0} to assignment Project: {1}" +
+                     " is authorize. Total cost:  {2:C}   ", codeName.Text, assignname.Text, cost);
+                    }
+
+
+
+            }//end of final stage
+                       
+
 
         }
     }
