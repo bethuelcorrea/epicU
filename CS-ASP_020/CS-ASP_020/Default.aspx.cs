f80d17b068822b18c52ed8324e7b98295deaf9dc
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-21 21:03:43 -0500

    ViewState

---
+++ CS-ASP_020/CS-ASP_020/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_020
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ViewState.Add("MyValue", "");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string value = ViewState["MyValue"].ToString();
+            value += serverControl.Text + "";
+            ViewState["MyValue"] = value;
+            lbltext.Text = value;
+            serverControl.Text = "";
+        }
+    }
+}
