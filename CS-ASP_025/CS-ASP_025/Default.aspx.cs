e7743153be5a1bc91d9229ecdfc2667ee52d2dd2
Bethuel Correa <bethuelcorrea@hotmail.com>
2016-09-24 10:27:39 -0500

    loops

---
+++ CS-ASP_025/CS-ASP_025/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_025
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            /*
+            if(!firstCheckBox.Checked)return;
+            if(!secondCheckBox.Checked)return;
+            if(!thirdCheckBox.Checked)return;
+
+            resultLable.Text = "they are all checked";
+
+            
+            a,b,c
+            if(new [] {a,b,c}.All(c =>c.Checked)){
+            
+            } 
+            
+            
+            */
+
+        }
+    }
+}
---
+++ CS-ASP_026/CS-ASP_026/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_026
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*
+            string result = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                result += "<br/>" + i.ToString();
+
+            }
+
+            lbltext.Text = result;
+           
+           */
+            /*
+             string result = "";
+
+             for (int i = 0; i < 10; i--)
+             {
+
+                 result += "<br/>" + i.ToString();
+
+             }
+
+             lbltext.Text = result;
+               */
+            /*
+         string[]
+
+         for (int i = 0; i < names.length; i++)
+         {
+          result = String.Format();
+          break;
+         }
+         */
+
+            //linq
+
+
+            var names = new[] { "a","b","c"};
+
+            var result = names
+                .FirstOrDefault(n => n == "a")
+                .Select(n=>string.Format("result{0}",n));
+
+
+        }
+    }
+}
